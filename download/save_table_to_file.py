2155fc194fac5c0cd1a9a7c6c63786b6b4f830de
Ane Rahbek Vierø <OA03FG@plan.aau.dk>
2020-12-05 21:50:00 +0100

    exporting edited shapefiles

--- download/save_table_to_file.py
+++ download/save_table_to_file.py
@@ -18,13 +18,18 @@ points_s_gdf = gpd.GeoDataFrame.from_postgis(retrieve_points_s, connection, geom
 points_i_gdf = gpd.GeoDataFrame.from_postgis(retrieve_points_i, connection, geom_col='geometry')
 # %%
 #Saving tables to geopackage
-ways_gdf.to_file("cycling_infra.gpkg", layer=ways_table, driver="GPKG")
-#%%
-points_s_gdf.to_file("points.gpkg", layer='points_service', driver="GPKG")
-points_i_gdf.to_file("points.gpkg", layer='points_infra', driver="GPKG")
+#ways_gdf.to_file("cycling_infra.gpkg", layer=ways_table, driver="GPKG")
+#points_s_gdf.to_file("points.gpkg", layer='points_service', driver="GPKG")
+#points_i_gdf.to_file("points.gpkg", layer='points_infra', driver="GPKG")
+
+# %%
+#Renaming column names to be compatible with shapefile limitations
+ways_dict = {'cycleway:left':'c_w_left', 'cycleway:right':'c_w_r', 'cycleway:width':'c_width', 'cycleway:left:width':'c_l_width', 'cycleway:right:width':'c_r_width', 'cycleway:both:width':'c_b_width', 'cycleway:surface':'c_surface', 'maxspeed_advisory':'maxs_advised', 'motor_vehicle':'motorveh.', 'oneway_bicycle':'onewaybike', 'parking:lane':'p_lane','parking:lane:right':'p_lane_r','parking:lane:left':'p_lane_l','parking:lane:both':'p_l_both','public_transport':'p_trans','car_traffic':'cartraff.','cycling_infrastructure':'c_infra','cycling_infra_simple':'c_infra_s','along_street':'by_street','cy_infra_separated':'cycle_sep','cycling_friendly':'c_friendly','cycling_allowed':'c_allowed','cycling_against':'c_against','on_street_parking':'parking','surface_assumed':'surface2'}
+ways_gdf2 = ways_gdf.rename(columns=ways_dict, inplace = False)
+
 # %%
 #Saving tables to shapefile
-ways_gdf.to_file("%s.shp" % ways_table)
+ways_gdf2.to_file("%s.shp" % ways_table)
+#%%
 points_s_gdf.to_file('points_service.shp')
 points_i_gdf.to_file('points_infra.shp')
-# %%
