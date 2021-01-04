769e976b2d03f0b9c9d0d37980fe10ecc9f32adf
anerv <vieroe@hotmail.com>
2021-01-04 16:33:04 +0100

    minor edits

--- download_classify/python/A5_save_table_to_file.py
+++ download_classify/python/A5_save_table_to_file.py
@@ -1,4 +1,4 @@
-#Save tables to file
+#Save tables to file (either Shape or Geopackage)
 #%%
 #Importing modules
 from config import *
@@ -14,15 +14,27 @@ retrieve_ways = "SELECT * FROM %s;" % ways_table
 retrieve_points_s = "SELECT * FROM points_service;"
 retrieve_points_i = "SELECT * FROM points_infra;"
 retrieve_poly_s = "SELECT * FROM bicycle_service;"
-
-#OBS ADD BICYCLE SERVICE
-
+#%%
 ways_gdf = gpd.GeoDataFrame.from_postgis(retrieve_ways, connection, geom_col='geometry')
 points_s_gdf = gpd.GeoDataFrame.from_postgis(retrieve_points_s, connection, geom_col='geometry')
 points_i_gdf = gpd.GeoDataFrame.from_postgis(retrieve_points_i, connection, geom_col='geometry')
 service_poly = gpd.GeoDataFrame.from_postgis(retrieve_poly_s, connection, geom_col='geometry')
 
 # %%
+#Saving tables to geopackage
+#Drop columns to simplify file
+drop_cols = ['flashing_lights','maxspeed:advisory','moped','motorcar','motor_vehicle','parking:lane','parking:lane:right','parking:lane:left','parking:lane:both','public_transport','ref','service','source:maxspeed','z_order','way_area']
+ways_gdf.drop(columns=drop_cols, inplace=True)
+#%%
+two_levels_up = str(Path(__file__).parents[1])
+fp_gpkg = two_levels_up + '\\data\\' + 'cycling_infra.gpkg'
+
+ways_gdf.to_file(fp_gpkg, layer=ways_table, driver="GPKG")
+points_s_gdf.to_file(fp_gpkg, layer='points_service', driver="GPKG")
+points_i_gdf.to_file(fp_gpkg, layer='points_infra', driver="GPKG")
+service_poly.to_file(fp_gpkg, layer='poly_service', driver="GPKG")
+#%%
+#If a shapefile is needed:
 #Renaming column names to be compatible with shapefile limitations
 ways_dict = {'cycleway:left':'c_w_left', 'cycleway:right':'c_w_r', 'cycleway:width':'c_width', 'cycleway:left:width':'c_l_width', 'cycleway:right:width':'c_r_width', 'cycleway:both:width':'c_b_width', 'cycleway:surface':'c_surface', 'maxspeed:advisory':'maxs_advised', 'motor_vehicle':'motorveh.', 'oneway_bicycle':'onewaybike', 'parking:lane':'p_lane','parking:lane:right':'p_lane_r','parking:lane:left':'p_lane_l','parking:lane:both':'p_l_both','public_transport':'p_trans','car_traffic':'cartraff.','cycling_infrastructure':'c_infra','cycling_infra_simple':'c_infra_s','along_street':'by_street','cy_infra_separated':'cycle_sep','cycling_friendly':'c_friendly','cycling_allowed':'c_allowed','cycling_against':'c_against','pedestrian_allowed':'ped_yes','on_street_park':'parking','surface_assumed':'surface2'}
 ways_gdf2 = ways_gdf.rename(columns=ways_dict, inplace = False)
@@ -32,17 +44,13 @@ drop_cols = ['flashing_lights','maxs_advised','moped','motorcar','motorveh.','p_
 ways_gdf2.drop(columns=drop_cols, inplace=True)
 # %%
 #Saving tables to shapefile
-two_levels_up = str(Path(__file__).parents[1])
 fp_ways = two_levels_up + '\\data\\' + ways_table + '.shp'
 fp_points_i = two_levels_up + '\\data\\' + 'points_infra.shp'
 fp_points_s = two_levels_up + '\\data\\' + 'points_service.shp'
-
+#%%
 ways_gdf2.to_file(fp_ways)
 points_i_gdf.to_file(fp_points_i)
 points_s_gdf.to_file(fp_points_s)
 #%%
-#Saving tables to geopackage
-#ways_gdf2.to_file("cycling_infra.gpkg", layer=ways_table, driver="GPKG")
-#points_s_gdf.to_file("points.gpkg", layer='points_service', driver="GPKG")
-#points_i_gdf.to_file("points.gpkg", layer='points_infra', driver="GPKG")
+
 #%%
