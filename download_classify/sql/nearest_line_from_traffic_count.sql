7110a8f9d39b1e17d1ae70435259f0dc404a415a
anerv <vieroe@hotmail.com>
2021-01-21 13:40:00 +0100

    formatting

--- download_classify/sql/nearest_line_from_traffic_count.sql
+++ download_classify/sql/nearest_line_from_traffic_count.sql
@@ -15,21 +15,24 @@ CREATE VIEW counts_car AS (SELECT DISTINCT ON (t.id)
     FROM traffic_counts AS t
     LEFT JOIN ways_rh AS w ON ST_DWithin(t.geometry,w.geometry,10) 
 	WHERE w.car_traffic = 'yes' AND t."KOERETOEJS" = 'MOTORKTJ'
-    ORDER BY t.id, dist);
+    ORDER BY t.id, dist)
+;
 
 CREATE VIEW counts_bike AS (SELECT DISTINCT ON (t.id)
     t.id AS point, t.geometry, t."AADT", w.name AS closest_way, w.osm_id, ST_Distance(t.geometry,w.geometry) AS dist 
     FROM traffic_counts AS t
     LEFT JOIN ways_rh AS w ON ST_DWithin(t.geometry,w.geometry,10) 
 	WHERE w.cycling_infrastructure IS NOT NULL AND t."KOERETOEJS" IN('CYKLER','C/K')
-    ORDER BY t.id, dist);
+    ORDER BY t.id, dist)
+;
 
 CREATE VIEW counts_bike_road AS (SELECT DISTINCT ON (t.id)
     t.id AS point, t.geometry, t."AADT", w.name AS closest_way, w.osm_id, ST_Distance(t.geometry,w.geometry) AS dist 
     FROM traffic_counts AS t
     LEFT JOIN ways_rh AS w ON ST_DWithin(t.geometry,w.geometry,10) 
 	WHERE t."KOERETOEJS" IN('CYKLER','C/K')
-    ORDER BY t.id, dist);
+    ORDER BY t.id, dist)
+;
     
 -- Update way based on view
 UPDATE ways_rh w SET car_aadt = "AADT" FROM counts_car c
@@ -37,12 +40,13 @@ UPDATE ways_rh w SET car_aadt = "AADT" FROM counts_car c
 
 -- Cycling counts added to cycling infrastructure
 UPDATE ways_rh w SET bike_aadt = "AADT" FROM counts_bike c
-    WHERE w.osm_id = c.osm_id;
+    WHERE w.osm_id = c.osm_id
+;
 
 -- Cycling counts added to nearest way regardless of type
 UPDATE ways_rh w SET bike_aadt = "AADT" FROM counts_bike_road c
-    WHERE w.osm_id = c.osm_id;
-
+    WHERE w.osm_id = c.osm_id
+;
 
 -- This table also contains data about average speed and speed limit
 CREATE VIEW speed AS (SELECT DISTINCT ON (t.id)
@@ -50,15 +54,18 @@ CREATE VIEW speed AS (SELECT DISTINCT ON (t.id)
     FROM traffic_counts AS t
     LEFT JOIN ways_rh AS w ON ST_DWithin(t.geometry,w.geometry,10)
     WHERE w.car_traffic = 'yes' AND  t."KOERETOEJS" = 'MOTORKTJ'
-    ORDER BY t.id, dist);
+    ORDER BY t.id, dist)
+;
 
 ALTER TABLE ways_rh ADD COLUMN ave_speed NUMERIC;
 
 UPDATE ways_rh w SET maxspeed = "HAST_GRAEN" FROM speed s
-    WHERE w.osm_id = s.osm_id AND maxspeed IS NULL;
+    WHERE w.osm_id = s.osm_id AND maxspeed IS NULL
+;
 
 UPDATE ways_rh w SET ave_speed = "GNS_HASTIG" FROM speed s 
-    WHERE w.osm_id = s.osm_id;
+    WHERE w.osm_id = s.osm_id
+;
 
 
 DROP VIEW counts_car;
